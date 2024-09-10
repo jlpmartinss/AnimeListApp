705e0099534744f70575d569d5ffd86a44c3b521
jlpmartinss <jm8244936@gmail.com>
2024-09-10 01:46:04 +0100

    view model created

---
+++ app/src/main/java/com/example/animelistapp/ui/screen/trending_anime/TrendingAnimeListViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.animelistapp.ui.screen.trending_anime
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.animelistapp.domain.model.AnimeData
+import com.example.animelistapp.domain.repository.KitsuRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TrendingAnimeListViewModel @Inject constructor(
+    private val repository: KitsuRepository
+) : ViewModel() {
+
+    private var _animeList = MutableStateFlow<List<AnimeData>>(emptyList())
+    val animeList = _animeList.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _animeList.update {
+                repository.getTrendingAnimeList()
+            }
+        }
+    }
+}
